ebb5c6a492ea623e2ba0ba2f987a35979f8b2798
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-08-21 23:44:50 +0530

    winner set

--- js/simple.js
+++ js/simple.js
@@ -1,10 +1,16 @@
 var refreshBtn = document.querySelector('#refresh');
 var resetBtn = document.querySelector('#reset');
+var header = document.querySelector('#header');
 refreshBtn.onclick = function() {
     var randomNumber1 = Math.floor(Math.random() * 6) + 1;
     var randomNumber2 = Math.floor(Math.random() * 6) + 1;
     while (randomNumber1 === randomNumber2) {
         randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    }
+    if (randomNumber1 > randomNumber2) {
+        header.textContent = 'Left Won';
+    } else {
+        header.textContent = 'Right Won';
     }
     document.querySelector('#left').setAttribute('src', 'images/dice' + randomNumber1 + '.png');
     document.querySelector('#right').setAttribute('src', 'images/dice' + randomNumber2 + '.png');
