de42003a3334a5d89d2a697c5aa734099730ae79
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-08-21 23:36:51 +0530

    reset set

--- js/simple.js
+++ js/simple.js
@@ -1,7 +1,17 @@
-var readyGo = document.querySelector('#refresh');
-readyGo.onclick = function() {
+var refreshBtn = document.querySelector('#refresh');
+var resetBtn = document.querySelector('#reset');
+refreshBtn.onclick = function() {
     var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-    document.querySelector('#left').setAttribute('src', 'images/dice' + randomNumber1 + '.png');
     var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    while (randomNumber1 === randomNumber2) {
+        randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    }
+    document.querySelector('#left').setAttribute('src', 'images/dice' + randomNumber1 + '.png');
+    document.querySelector('#right').setAttribute('src', 'images/dice' + randomNumber2 + '.png');
+}
+resetBtn.onclick = function() {
+    var randomNumber1 = 1;
+    document.querySelector('#left').setAttribute('src', 'images/dice' + randomNumber1 + '.png');
+    var randomNumber2 = 1;
     document.querySelector('#right').setAttribute('src', 'images/dice' + randomNumber2 + '.png');
 }
