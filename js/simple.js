4d1e4a5ca3193ead3c139f48ae6ad5ab30ae024e
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-08-22 00:05:38 +0530

    final fix

--- js/simple.js
+++ js/simple.js
@@ -1,5 +1,6 @@
 var refreshBtn = document.querySelector('#refresh');
 var resetBtn = document.querySelector('#reset');
+var home = document.querySelector('#home');
 var header = document.querySelector('#header');
 refreshBtn.onclick = function() {
     var randomNumber1 = Math.floor(Math.random() * 6) + 1;
@@ -16,9 +17,18 @@ refreshBtn.onclick = function() {
     document.querySelector('#right').setAttribute('src', 'images/dice' + randomNumber2 + '.png');
 }
 resetBtn.onclick = function() {
+    home.setAttribute('class', 'nav-link active');
     header.textContent = 'Pick A Side';
     var randomNumber1 = 1;
     document.querySelector('#left').setAttribute('src', 'images/dice' + randomNumber1 + '.png');
     var randomNumber2 = 1;
     document.querySelector('#right').setAttribute('src', 'images/dice' + randomNumber2 + '.png');
+}
+resetBtn.onmouseover = function() {
+    resetBtn.setAttribute('class', 'nav-link active');
+    home.setAttribute('class', 'nav-link');
+}
+home.onmouseout = function() {
+    resetBtn.setAttribute('class', 'nav-link ');
+    home.setAttribute('class', 'nav-link active');
 }
